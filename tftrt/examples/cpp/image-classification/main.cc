e629e369af78610abec0e62cf6ca5d64aa8eaad0
Vinh Nguyen <vinh.nguyenx@gmail.com>
2019-12-12 10:03:35 +1100

    edit copyrights

--- tftrt/examples/cpp/image-classification/main.cc
+++ tftrt/examples/cpp/image-classification/main.cc
@@ -13,7 +13,23 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-// A minimal but useful C++ example showing how to load an Imagenet-style object
+/*
+# Copyright 2019 NVIDIA Corporation. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================*/
+
+// A minimal but useful C++ example showing how to load a TF-TRT optimized object
 // recognition TensorFlow model, prepare input images for it, run them through
 // the graph, and interpret the results.
 //
