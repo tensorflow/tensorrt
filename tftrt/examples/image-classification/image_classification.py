483d64c408b2cfb015898c1cdb415ffe2b7effa1
Jonathan Dekhtiar <jdekhtiar@nvidia.com>
2021-06-03 14:09:46 -0700

    Cosmetic changes

--- tftrt/examples/image-classification/image_classification.py
+++ tftrt/examples/image-classification/image_classification.py
@@ -36,7 +36,6 @@ import preprocessing
 
 logging.disable(logging.WARNING)
 
-
 SAMPLES_IN_VALIDATION_SET = 50000
 
 
@@ -51,7 +50,8 @@ def deserialize_image_record(record):
         'image/object/bbox/ymax': tf.io.VarLenFeature(dtype=tf.float32)
     }
     with tf.compat.v1.name_scope('deserialize_image_record'):
-        obj = tf.io.parse_single_example(serialized=record, features=feature_map)
+        obj = tf.io.parse_single_example(serialized=record,
+                                         features=feature_map)
         imgdata = obj['image/encoded']
         label = tf.cast(obj['image/class/label'], tf.int32)
     return imgdata, label
@@ -171,7 +171,7 @@ def get_graph_func(input_saved_model_dir,
                 if i >= build_steps:
                     break
 
-                print("* [%s] - step %d/%d" % (model_phase, i+1, build_steps))
+                print("* [%s] - step %d/%d" % (model_phase, i + 1, build_steps))
                 yield batch_images,
 
         if conversion_params.precision_mode != 'INT8':
@@ -188,7 +188,8 @@ def get_graph_func(input_saved_model_dir,
         else:
             print('Graph conversion and INT8 calibration...')
             converter.convert(calibration_input_fn=partial(
-                input_fn, calib_files, num_calib_inputs//batch_size, "Calibration"))
+                input_fn, calib_files, num_calib_inputs // batch_size,
+                "Calibration"))
             if optimize_offline:
                 print('Building TensorRT engines...')
                 converter.build(
@@ -247,9 +248,9 @@ def run_inference(graph_func,
 
         if (i + 1) % display_every == 0:
             print("  step %d/%d, iter_time(ms)=%.0f" % (
-                i+1,
+                i + 1,
                 SAMPLES_IN_VALIDATION_SET // batch_size,
-                iter_times[-1]*1000
+                iter_times[-1] * 1000
             ))
 
         corrects += eval_fn(batch_preds, batch_labels.numpy(), adjust)
@@ -276,196 +277,201 @@ def run_inference(graph_func,
 
 
 def config_gpu_memory(gpu_mem_cap):
-  gpus = tf.config.experimental.list_physical_devices('GPU')
-  if not gpus:
-    return
-  print('Found the following GPUs:')
-  for gpu in gpus:
-    print(' ', gpu)
-  for gpu in gpus:
-    try:
-      if not gpu_mem_cap:
-        tf.config.experimental.set_memory_growth(gpu, True)
-      else:
-        tf.config.experimental.set_virtual_device_configuration(
-            gpu,
-            [tf.config.experimental.VirtualDeviceConfiguration(
-               memory_limit=gpu_mem_cap)])
-    except RuntimeError as e:
-      print('Can not set GPU memory config', e)
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if not gpus:
+        return
+    print('Found the following GPUs:')
+    for gpu in gpus:
+        print(' ', gpu)
+    for gpu in gpus:
+        try:
+            if not gpu_mem_cap:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            else:
+                tf.config.experimental.set_virtual_device_configuration(
+                    gpu,
+                    [tf.config.experimental.VirtualDeviceConfiguration(
+                        memory_limit=gpu_mem_cap)])
+        except RuntimeError as e:
+            print('Can not set GPU memory config', e)
 
 
 def get_trt_conversion_params(max_workspace_size_bytes,
                               precision_mode,
                               minimum_segment_size):
-  conversion_params = trt.DEFAULT_TRT_CONVERSION_PARAMS
-  conversion_params = conversion_params._replace(
-      max_workspace_size_bytes=max_workspace_size_bytes)
-  conversion_params = conversion_params._replace(precision_mode=precision_mode)
-  conversion_params = conversion_params._replace(
-      minimum_segment_size=minimum_segment_size)
-  conversion_params = conversion_params._replace(
-      use_calibration=precision_mode == 'INT8')
-  return conversion_params
+    conversion_params = trt.DEFAULT_TRT_CONVERSION_PARAMS
+    conversion_params = conversion_params._replace(
+        max_workspace_size_bytes=max_workspace_size_bytes)
+    conversion_params = conversion_params._replace(
+        precision_mode=precision_mode)
+    conversion_params = conversion_params._replace(
+        minimum_segment_size=minimum_segment_size)
+    conversion_params = conversion_params._replace(
+        use_calibration=precision_mode == 'INT8')
+    return conversion_params
 
 
 if __name__ == '__main__':
-  logging.getLogger("tensorflow").setLevel(logging.INFO)
+    logging.getLogger("tensorflow").setLevel(logging.INFO)
 
-  parser = argparse.ArgumentParser(description='Evaluate model')
-  parser.add_argument('--input_saved_model_dir', type=str, default=None,
-                      help='Directory containing the input saved model.')
-  parser.add_argument('--output_saved_model_dir', type=str, default=None,
-                      help='Directory in which the converted model is saved')
-  parser.add_argument('--preprocess_method', type=str,
-                      choices=['vgg', 'inception'], default='vgg',
-                      help='The image preprocessing method')
-  parser.add_argument('--input_size', type=int, default=224,
-                      help='Size of input images expected by the model')
-  parser.add_argument('--num_classes', type=int, default=1001,
-                      help='Number of classes used when training the model')
-  parser.add_argument('--data_dir', type=str, default=None,
-                      help='Directory containing validation set'
-                      'TFRecord files.')
-  parser.add_argument('--calib_data_dir', type=str,
-                      help='Directory containing TFRecord files for'
-                      'calibrating INT8.')
-  parser.add_argument('--use_trt', action='store_true',
-                      help='If set, the graph will be converted to a'
-                      'TensorRT graph.')
-  parser.add_argument('--optimize_offline', action='store_true',
-                      help='If set, TensorRT engines are built'
-                      'before runtime.')
-  parser.add_argument('--precision', type=str,
-                      choices=['FP32', 'FP16', 'INT8'], default='FP32',
-                      help='Precision mode to use. FP16 and INT8 only'
-                      'work in conjunction with --use_trt')
-  parser.add_argument('--batch_size', type=int, default=8,
-                      help='Number of images per batch.')
-  parser.add_argument('--minimum_segment_size', type=int, default=2,
-                      help='Minimum number of TF ops in a TRT engine.')
-  parser.add_argument('--num_iterations', type=int, default=None,
-                      help='How many iterations(batches) to evaluate.'
-                      'If not supplied, the whole set will be evaluated.')
-  parser.add_argument('--display_every', type=int, default=100,
-                      help='Number of iterations executed between'
-                      'two consecutive display of metrics')
-  parser.add_argument('--use_synthetic', action='store_true',
-                      help='If set, one batch of random data is'
-                      'generated and used at every iteration.')
-  parser.add_argument('--num_warmup_iterations', type=int, default=50,
-                      help='Number of initial iterations skipped from timing')
-  parser.add_argument('--num_calib_inputs', type=int, default=500,
-                      help='Number of inputs (e.g. images) used for'
-                      'calibration (last batch is skipped in case'
-                      'it is not full)')
-  parser.add_argument('--gpu_mem_cap', type=int, default=0,
-                      help='Upper bound for GPU memory in MB.'
-                      'Default is 0 which means allow_growth will be used.')
-  parser.add_argument('--max_workspace_size', type=int, default=(1<<30),
-                      help='workspace size in bytes')
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Evaluate model')
+    parser.add_argument('--input_saved_model_dir', type=str, default=None,
+                        help='Directory containing the input saved model.')
+    parser.add_argument('--output_saved_model_dir', type=str, default=None,
+                        help='Directory in which the converted model is saved')
+    parser.add_argument('--preprocess_method', type=str,
+                        choices=['vgg', 'inception'], default='vgg',
+                        help='The image preprocessing method')
+    parser.add_argument('--input_size', type=int, default=224,
+                        help='Size of input images expected by the model')
+    parser.add_argument('--num_classes', type=int, default=1001,
+                        help='Number of classes used when training the model')
+    parser.add_argument('--data_dir', type=str, default=None,
+                        help='Directory containing validation set'
+                             'TFRecord files.')
+    parser.add_argument('--calib_data_dir', type=str,
+                        help='Directory containing TFRecord files for'
+                             'calibrating INT8.')
+    parser.add_argument('--use_trt', action='store_true',
+                        help='If set, the graph will be converted to a'
+                             'TensorRT graph.')
+    parser.add_argument('--optimize_offline', action='store_true',
+                        help='If set, TensorRT engines are built'
+                             'before runtime.')
+    parser.add_argument('--precision', type=str,
+                        choices=['FP32', 'FP16', 'INT8'], default='FP32',
+                        help='Precision mode to use. FP16 and INT8 only'
+                             'work in conjunction with --use_trt')
+    parser.add_argument('--batch_size', type=int, default=8,
+                        help='Number of images per batch.')
+    parser.add_argument('--minimum_segment_size', type=int, default=2,
+                        help='Minimum number of TF ops in a TRT engine.')
+    parser.add_argument('--num_iterations', type=int, default=None,
+                        help='How many iterations(batches) to evaluate. If not '
+                             'supplied, the whole set will be evaluated.')
+    parser.add_argument('--display_every', type=int, default=100,
+                        help='Number of iterations executed between'
+                             'two consecutive display of metrics')
+    parser.add_argument('--use_synthetic', action='store_true',
+                        help='If set, one batch of random data is'
+                             'generated and used at every iteration.')
+    parser.add_argument('--num_warmup_iterations', type=int, default=50,
+                        help='Number of initial iterations skipped from timing')
+    parser.add_argument('--num_calib_inputs', type=int, default=500,
+                        help='Number of inputs (e.g. images) used for'
+                             'calibration (last batch is skipped in case'
+                             'it is not full)')
+    parser.add_argument('--gpu_mem_cap', type=int, default=0,
+                        help='Upper bound for GPU memory in MB. Default is 0 '
+                             'which means allow_growth will be used.')
+    parser.add_argument('--max_workspace_size', type=int, default=(1 << 30),
+                        help='workspace size in bytes')
+    args = parser.parse_args()
 
-  if args.precision != 'FP32' and not args.use_trt:
-    raise ValueError('TensorRT must be enabled for FP16'
-                     'or INT8 modes (--use_trt).')
+    if args.precision != 'FP32' and not args.use_trt:
+        raise ValueError('TensorRT must be enabled for FP16'
+                         'or INT8 modes (--use_trt).')
 
-  if args.precision == 'INT8' and not args.calib_data_dir:
-    raise ValueError('--calib_data_dir is required for INT8 mode')
+    if args.precision == 'INT8' and not args.calib_data_dir:
+        raise ValueError('--calib_data_dir is required for INT8 mode')
 
-  if args.use_synthetic:
-      if args.num_iterations is None:
-          args.num_iterations = SAMPLES_IN_VALIDATION_SET // args.batch_size
+    if args.use_synthetic:
+        if args.num_iterations is None:
+            args.num_iterations = SAMPLES_IN_VALIDATION_SET // args.batch_size
 
-  if (
-      args.num_iterations is not None and
-      args.num_iterations < args.num_warmup_iterations
-  ):
-      raise ValueError(
-          '--num_iterations must be larger than --num_warmup_iterations '
-          '({} <= {})'.format(args.num_iterations, args.num_warmup_iterations))
+    if (
+        args.num_iterations is not None and
+        args.num_iterations <= args.num_warmup_iterations
+    ):
+        raise ValueError(
+            '--num_iterations must be larger than --num_warmup_iterations '
+            '({} <= {})'.format(args.num_iterations,
+                                args.num_warmup_iterations))
 
-  if args.num_calib_inputs < args.batch_size:
-    raise ValueError(
-        '--num_calib_inputs must not be smaller than --batch_size'
-        '({} <= {})'.format(args.num_calib_inputs, args.batch_size))
+    if args.num_calib_inputs <= args.batch_size:
+        raise ValueError(
+            '--num_calib_inputs must not be smaller than --batch_size'
+            '({} <= {})'.format(args.num_calib_inputs, args.batch_size))
 
-  if args.data_dir is None:
-    raise ValueError("--data_dir is required")
+    if args.data_dir is None:
+        raise ValueError("--data_dir is required")
 
-  if args.use_trt and not args.output_saved_model_dir:
-    raise ValueError("--output_saved_model_dir must be set if use_trt=True")
+    if args.use_trt and not args.output_saved_model_dir:
+        raise ValueError("--output_saved_model_dir must be set if use_trt=True")
 
-  calib_files = []
 
-  def get_files(data_dir, filename_pattern):
-    if data_dir is None:
-      return []
+    def get_files(data_dir, filename_pattern):
+        if data_dir is None:
+            return []
 
-    files = tf.io.gfile.glob(os.path.join(data_dir, filename_pattern))
+        files = tf.io.gfile.glob(os.path.join(data_dir, filename_pattern))
 
-    if not files:
-      raise ValueError('Can not find any files in {} with '
-                       'pattern "{}"'.format(data_dir, filename_pattern))
-    return files
+        if not files:
+            raise ValueError('Can not find any files in {} with '
+                             'pattern "{}"'.format(data_dir, filename_pattern))
+        return files
 
-  data_files = get_files(args.data_dir, 'validation*')
 
-  if args.precision == 'INT8':
-    calib_files = get_files(args.calib_data_dir, 'train*')
+    data_files = get_files(args.data_dir, 'validation*')
 
-  config_gpu_memory(args.gpu_mem_cap)
+    calib_files = (
+        []
+        if args.precision != 'INT8' else
+        get_files(args.calib_data_dir, 'train*')
+    )
 
-  params = get_trt_conversion_params(
-      args.max_workspace_size,
-      args.precision,
-      args.minimum_segment_size
-  )
+    config_gpu_memory(args.gpu_mem_cap)
 
-  graph_func, times = get_graph_func(
-      input_saved_model_dir=args.input_saved_model_dir,
-      output_saved_model_dir=args.output_saved_model_dir,
-      preprocess_method=args.preprocess_method,
-      input_size=args.input_size,
-      conversion_params=params,
-      use_trt=args.use_trt,
-      calib_files=calib_files,
-      batch_size=args.batch_size,
-      num_calib_inputs=args.num_calib_inputs,
-      optimize_offline=args.optimize_offline
-  )
+    params = get_trt_conversion_params(
+        args.max_workspace_size,
+        args.precision,
+        args.minimum_segment_size
+    )
 
-  def print_dict(input_dict, prefix='  ', postfix=''):
-    for k, v in sorted(input_dict.items()):
-      print('{}{}: {}{}'.format(
-          prefix, k, '%.1f' % v
-          if isinstance(v, float) else v, postfix
-      ))
+    graph_func, times = get_graph_func(
+        input_saved_model_dir=args.input_saved_model_dir,
+        output_saved_model_dir=args.output_saved_model_dir,
+        preprocess_method=args.preprocess_method,
+        input_size=args.input_size,
+        conversion_params=params,
+        use_trt=args.use_trt,
+        calib_files=calib_files,
+        batch_size=args.batch_size,
+        num_calib_inputs=args.num_calib_inputs,
+        optimize_offline=args.optimize_offline
+    )
 
-  print('Benchmark arguments:')
-  print_dict(vars(args))
-  print('TensorRT Conversion Params:')
-  print_dict(dict(params._asdict()))
-  print('Conversion times:')
-  print_dict(times, postfix='s')
+    def print_dict(input_dict, prefix='  ', postfix=''):
+        for k, v in sorted(input_dict.items()):
+            print('{}{}: {}{}'.format(
+                prefix, k, '%.1f' % v
+                if isinstance(v, float) else v, postfix
+            ))
 
-  results = run_inference(
-      graph_func,
-      data_files=data_files,
-      batch_size=args.batch_size,
-      num_iterations=args.num_iterations,
-      num_warmup_iterations=args.num_warmup_iterations,
-      preprocess_method=args.preprocess_method,
-      input_size=args.input_size,
-      num_classes=args.num_classes,
-      use_synthetic=args.use_synthetic,
-      display_every=args.display_every
-  )
+    print('Benchmark arguments:')
+    print_dict(vars(args))
+    print('TensorRT Conversion Params:')
+    print_dict(dict(params._asdict()))
+    print('Conversion times:')
+    print_dict(times, postfix='s')
 
-  print('  accuracy: %.2f' % (results['accuracy'] * 100))
-  print('  images/sec: %d' % results['images_per_sec'])
-  print('  99th_percentile(ms): %.2f' % results['99th_percentile'])
-  print('  total_time(s): %.1f' % results['total_time'])
-  print('  latency_mean(ms): %.2f' % results['latency_mean'])
-  print('  latency_median(ms): %.2f' % results['latency_median'])
-  print('  latency_min(ms): %.2f' % results['latency_min'])
+    results = run_inference(
+        graph_func,
+        data_files=data_files,
+        batch_size=args.batch_size,
+        num_iterations=args.num_iterations,
+        num_warmup_iterations=args.num_warmup_iterations,
+        preprocess_method=args.preprocess_method,
+        input_size=args.input_size,
+        num_classes=args.num_classes,
+        use_synthetic=args.use_synthetic,
+        display_every=args.display_every
+    )
+
+    print('  accuracy: %.2f' % (results['accuracy'] * 100))
+    print('  images/sec: %d' % results['images_per_sec'])
+    print('  99th_percentile(ms): %.2f' % results['99th_percentile'])
+    print('  total_time(s): %.1f' % results['total_time'])
+    print('  latency_mean(ms): %.2f' % results['latency_mean'])
+    print('  latency_median(ms): %.2f' % results['latency_median'])
+    print('  latency_min(ms): %.2f' % results['latency_min'])
