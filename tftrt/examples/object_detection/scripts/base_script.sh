cd6bc484f58977a83274aff879d0b5b14b4bf121
Jonathan Dekhtiar <jdekhtiar@nvidia.com>
2021-09-09 19:05:51 -0700

    Num Iterations Added to Benchmark Scripts

--- tftrt/examples/object_detection/scripts/base_script.sh
+++ tftrt/examples/object_detection/scripts/base_script.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-MODEL_NAME=""
-BATCH_SIZE=8
-
 # Runtime Parameters
-USE_INPUT_SIGNATURE_KEY=0
-INPUT_SIGNATURE_KEY=""
-
-USE_SYNTHETIC_DATA=0
-SKIP_ACCURACY_TESTING=0
-
+MODEL_NAME=""
 DATA_DIR=""
 MODEL_DIR=""
 
+BATCH_SIZE=8
+NUM_ITERATIONS_FLAG=""
+
 # TF-TRT Parameters
-USE_XLA=0
-USE_TF32=1
 USE_TFTRT=0
 TFTRT_PRECISION="FP32"
-USE_DYNAMIC_SHAPE=0
+
+# Default Argument Values
+TF_XLA_FLAGS=""
+NVIDIA_TF32_OVERRIDE=""
+USE_SYNTHETIC_DATA_FLAG=""
+USE_DYNAMIC_SHAPE_FLAG=""
+SKIP_ACCURACY_TESTING_FLAG=""
+INPUT_SIGNATURE_KEY_FLAG=""
 
 # Loop through arguments and process them
 for arg in "$@"
@@ -29,11 +29,11 @@ do
         shift # Remove --model_name from processing
         ;;
         --use_xla)
-        USE_XLA=1
+        TF_XLA_FLAGS="TF_XLA_FLAGS=--tf_xla_auto_jit=2"
         shift # Remove --use_xla from processing
         ;;
         --no_tf32)
-        USE_TF32=0
+        NVIDIA_TF32_OVERRIDE="NVIDIA_TF32_OVERRIDE=0"
         shift # Remove --no_tf32 from processing
         ;;
         --batch_size=*)
@@ -57,60 +57,27 @@ do
         shift # Remove --tftrt_precision= from processing
         ;;
         --use_synthetic_data)
-        USE_SYNTHETIC_DATA=1
+        USE_SYNTHETIC_DATA_FLAG="--use_synthetic_data"
         shift # Remove --use_synthetic_data from processing
         ;;
         --use_dynamic_shape)
-        USE_DYNAMIC_SHAPE=1
+        USE_DYNAMIC_SHAPE_FLAG="--use_dynamic_shape"
         shift # Remove --use_dynamic_shape from processing
         ;;
         --skip_accuracy_testing)
-        SKIP_ACCURACY_TESTING=1
+        SKIP_ACCURACY_TESTING_FLAG="--skip_accuracy_testing"
         shift # Remove --skip_accuracy_testing from processing
         ;;
         --input_signature_key=*)
-        USE_INPUT_SIGNATURE_KEY=1
-        INPUT_SIGNATURE_KEY="${arg#*=}"
+        INPUT_SIGNATURE_KEY_FLAG="--input_signature_key=${arg#*=}"
         shift # Remove --input_signature_key from processing
+        ;;
+        --num_iterations=*)
+        NUM_ITERATIONS_FLAG="--num_iterations=${arg#*=}"
+        shift # Remove --num_iterations from processing
         ;;
     esac
 done
-
-if [[ ${USE_XLA} == "1" ]]; then
-    TF_XLA_FLAGS="TF_XLA_FLAGS=--tf_xla_auto_jit=2"
-else
-    TF_XLA_FLAGS=""
-fi
-
-if [[ ${USE_TF32} == "0" ]]; then
-    NVIDIA_TF32_OVERRIDE="NVIDIA_TF32_OVERRIDE=0"
-else
-    NVIDIA_TF32_OVERRIDE=""
-fi
-
-if [[ ${USE_SYNTHETIC_DATA} == "1" ]]; then
-    USE_SYNTHETIC_DATA_FLAG="--use_synthetic_data"
-else
-    USE_SYNTHETIC_DATA_FLAG=""
-fi
-
-if [[ ${USE_DYNAMIC_SHAPE} == "1" ]]; then
-    USE_DYNAMIC_SHAPE_FLAG="--use_dynamic_shape"
-else
-    USE_DYNAMIC_SHAPE_FLAG=""
-fi
-
-if [[ ${SKIP_ACCURACY_TESTING} == "1" ]]; then
-    SKIP_ACCURACY_TESTING_FLAG="--skip_accuracy_testing"
-else
-    SKIP_ACCURACY_TESTING_FLAG=""
-fi
-
-if [[ ${USE_INPUT_SIGNATURE_KEY} == "1" ]]; then
-    INPUT_SIGNATURE_KEY_FLAG="--input_signature_key=${INPUT_SIGNATURE_KEY}"
-else
-    INPUT_SIGNATURE_KEY_FLAG=""
-fi
 
 # ============== Set model specific parameters ============= #
 
@@ -132,28 +99,24 @@ echo ""
 echo "[*] DATA_DIR: ${DATA_DIR}"
 echo "[*] MODEL_DIR: ${MODEL_DIR}"
 echo "[*] BATCH_SIZE: ${BATCH_SIZE}"
+echo "[*] NUM_ITERATIONS_FLAG: ${NUM_ITERATIONS_FLAG}"
 echo ""
-echo "[*] USE_XLA: ${USE_XLA}"
+echo "[*] NVIDIA_TF32_OVERRIDE: ${NVIDIA_TF32_OVERRIDE}"
 echo "[*] TF_XLA_FLAGS: ${TF_XLA_FLAGS}"
 echo ""
-echo "[*] USE_SYNTHETIC_DATA: ${USE_SYNTHETIC_DATA}"
-echo "[*] USE_SYNTHETIC_DATA_FLAG: ${USE_SYNTHETIC_DATA_FLAG}"
-echo ""
-echo "[*] SKIP_ACCURACY_TESTING: ${SKIP_ACCURACY_TESTING}"
-echo "[*] SKIP_ACCURACY_TESTING_FLAG: ${SKIP_ACCURACY_TESTING_FLAG}"
-echo ""
-echo "[*] USE_TF32: ${USE_TF32}"
-echo "[*] NVIDIA_TF32_OVERRIDE: ${NVIDIA_TF32_OVERRIDE}"
-echo ""
-echo "[*] USE_INPUT_SIGNATURE_KEY: ${USE_INPUT_SIGNATURE_KEY}"
 echo "[*] INPUT_SIGNATURE_KEY_FLAG: ${INPUT_SIGNATURE_KEY_FLAG}"
+echo "[*] SKIP_ACCURACY_TESTING_FLAG: ${SKIP_ACCURACY_TESTING_FLAG}"
+echo "[*] USE_SYNTHETIC_DATA_FLAG: ${USE_SYNTHETIC_DATA_FLAG}"
 echo ""
 echo "[*] USE_TFTRT: ${USE_TFTRT}"
 echo "[*] TFTRT_PRECISION: ${TFTRT_PRECISION}"
 echo "[*] MAX_WORKSPACE_SIZE: ${MAX_WORKSPACE_SIZE}"
 echo "[*] MIN_SEGMENT_SIZE: ${MIN_SEGMENT_SIZE}"
-echo "[*] USE_DYNAMIC_SHAPE: ${USE_DYNAMIC_SHAPE}"
 echo "[*] USE_DYNAMIC_SHAPE_FLAG: ${USE_DYNAMIC_SHAPE_FLAG}"
+echo ""
+echo "[*] INPUT_SIZE: ${INPUT_SIZE}"
+echo "[*] PREPROCESS_METHOD: ${PREPROCESS_METHOD}"
+echo "[*] NUM_CLASSES: ${NUM_CLASSES}"
 echo -e "********************************************************************\n"
 
 # ======================= ARGUMENT VALIDATION ======================= #
@@ -239,7 +202,8 @@ COMMAND="${PREPEND_COMMAND} python object_detection.py \
     ${USE_SYNTHETIC_DATA_FLAG} \
     ${INPUT_SIGNATURE_KEY_FLAG} \
     --input_size 640 \
-    --batch_size ${BATCH_SIZE}"
+    --batch_size ${BATCH_SIZE} \
+    ${NUM_ITERATIONS_FLAG}"
 
 if [[ ${USE_TFTRT} != "0" ]]; then
     COMMAND="${COMMAND} \
