fec6571c4195523ee3039e071b4d8bcb39300eda
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2021-10-18 14:15:50 -0700

    Simplified base_scripts

--- tftrt/examples/image_classification/scripts/base_script.sh
+++ tftrt/examples/image_classification/scripts/base_script.sh
@@ -5,20 +5,11 @@ MODEL_NAME=""
 DATA_DIR=""
 MODEL_DIR=""
 
-BATCH_SIZE=32
-NUM_ITERATIONS_FLAG=""
-
-# TF-TRT Parameters
-USE_TFTRT=0
-TFTRT_PRECISION="FP32"
-
 # Default Argument Values
 TF_XLA_FLAGS=""
 NVIDIA_TF32_OVERRIDE=""
-USE_SYNTHETIC_DATA_FLAG=""
-USE_DYNAMIC_SHAPE_FLAG=""
-SKIP_ACCURACY_TESTING_FLAG=""
-INPUT_SIGNATURE_KEY_FLAG=""
+
+BYPASS_ARGUMENTS=""
 
 # Loop through arguments and process them
 for arg in "$@"
@@ -36,10 +27,6 @@ do
         NVIDIA_TF32_OVERRIDE="NVIDIA_TF32_OVERRIDE=0"
         shift # Remove --no_tf32 from processing
         ;;
-        --batch_size=*)
-        BATCH_SIZE="${arg#*=}"
-        shift # Remove --batch_size= from processing
-        ;;
         --data_dir=*)
         DATA_DIR="${arg#*=}"
         shift # Remove --data_dir= from processing
@@ -48,33 +35,8 @@ do
         MODEL_DIR="${arg#*=}"
         shift # Remove --input_saved_model_dir= from processing
         ;;
-        --use_tftrt)
-        USE_TFTRT=1
-        shift # Remove --use_tftrt from processing
-        ;;
-        --precision=*)
-        TFTRT_PRECISION="${arg#*=}"
-        shift # Remove --precision= from processing
-        ;;
-        --use_synthetic_data)
-        USE_SYNTHETIC_DATA_FLAG="--use_synthetic_data"
-        shift # Remove --use_synthetic_data from processing
-        ;;
-        --use_dynamic_shape)
-        USE_DYNAMIC_SHAPE_FLAG="--use_dynamic_shape"
-        shift # Remove --use_dynamic_shape from processing
-        ;;
-        --skip_accuracy_testing)
-        SKIP_ACCURACY_TESTING_FLAG="--skip_accuracy_testing"
-        shift # Remove --skip_accuracy_testing from processing
-        ;;
-        --input_signature_key=*)
-        INPUT_SIGNATURE_KEY_FLAG="--input_signature_key=${arg#*=}"
-        shift # Remove --input_signature_key from processing
-        ;;
-        --num_iterations=*)
-        NUM_ITERATIONS_FLAG="--num_iterations=${arg#*=}"
-        shift # Remove --num_iterations from processing
+        *)
+        BYPASS_ARGUMENTS=" ${BYPASS_ARGUMENTS} ${arg}"
         ;;
     esac
 done
@@ -84,9 +46,6 @@ done
 INPUT_SIZE=224
 PREPROCESS_METHOD="vgg"
 NUM_CLASSES=1001
-
-MIN_SEGMENT_SIZE=2
-MAX_WORKSPACE_SIZE=$((2**32))
 
 case ${MODEL_NAME} in
   "inception_v3" | "inception_v4")
@@ -123,25 +82,16 @@ echo "[*] MODEL_NAME: ${MODEL_NAME}"
 echo ""
 echo "[*] DATA_DIR: ${DATA_DIR}"
 echo "[*] MODEL_DIR: ${MODEL_DIR}"
-echo "[*] BATCH_SIZE: ${BATCH_SIZE}"
-echo "[*] NUM_ITERATIONS_FLAG: ${NUM_ITERATIONS_FLAG}"
 echo ""
 echo "[*] NVIDIA_TF32_OVERRIDE: ${NVIDIA_TF32_OVERRIDE}"
 echo "[*] TF_XLA_FLAGS: ${TF_XLA_FLAGS}"
 echo ""
-echo "[*] INPUT_SIGNATURE_KEY_FLAG: ${INPUT_SIGNATURE_KEY_FLAG}"
-echo "[*] SKIP_ACCURACY_TESTING_FLAG: ${SKIP_ACCURACY_TESTING_FLAG}"
-echo "[*] USE_SYNTHETIC_DATA_FLAG: ${USE_SYNTHETIC_DATA_FLAG}"
-echo ""
-echo "[*] USE_TFTRT: ${USE_TFTRT}"
-echo "[*] TFTRT_PRECISION: ${TFTRT_PRECISION}"
-echo "[*] MAX_WORKSPACE_SIZE: ${MAX_WORKSPACE_SIZE}"
-echo "[*] MIN_SEGMENT_SIZE: ${MIN_SEGMENT_SIZE}"
-echo "[*] USE_DYNAMIC_SHAPE_FLAG: ${USE_DYNAMIC_SHAPE_FLAG}"
-echo ""
+# Custom Image Classification Task Flags
 echo "[*] INPUT_SIZE: ${INPUT_SIZE}"
 echo "[*] PREPROCESS_METHOD: ${PREPROCESS_METHOD}"
 echo "[*] NUM_CLASSES: ${NUM_CLASSES}"
+echo ""
+echo "[*] BYPASS_ARGUMENTS: ${BYPASS_ARGUMENTS}"
 echo -e "********************************************************************\n"
 
 # ======================= ARGUMENT VALIDATION ======================= #
@@ -177,14 +127,6 @@ if [[ ! -d ${INPUT_SAVED_MODEL_DIR} ]]; then
     exit 1
 fi
 
-# TFTRT Arguments
-
-ALLOWED_TFTRT_PRECISION="FP32 FP16 INT8"
-
-if ! $(echo ${ALLOWED_TFTRT_PRECISION} | grep -w ${TFTRT_PRECISION} > /dev/null); then
-    echo "ERROR: Unknown TFTRT_PRECISION received: \`${TFTRT_PRECISION}\`. [Allowed: ${ALLOWED_TFTRT_PRECISION}]"
-fi
-
 # %%%%%%%%%%%%%%%%%%%%%%% ARGUMENT VALIDATION %%%%%%%%%%%%%%%%%%%%%%% #
 
 BENCH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
@@ -198,26 +140,13 @@ COMMAND="${PREPEND_COMMAND} python image_classification.py \
     --data_dir ${DATA_DIR} \
     --calib_data_dir ${DATA_DIR} \
     --input_saved_model_dir ${INPUT_SAVED_MODEL_DIR} \
-    --num_warmup_iterations 100 \
-    --display_every 50 \
-    ${SKIP_ACCURACY_TESTING_FLAG} \
-    ${USE_SYNTHETIC_DATA_FLAG} \
-    ${INPUT_SIGNATURE_KEY_FLAG} \
-    --batch_size ${BATCH_SIZE} \
-    ${NUM_ITERATIONS_FLAG} \
+    --output_saved_model_dir /tmp/$RANDOM \
     --input_size ${INPUT_SIZE} \
     --preprocess_method ${PREPROCESS_METHOD} \
-    --num_classes ${NUM_CLASSES}"
+    --num_classes ${NUM_CLASSES} \
+    ${BYPASS_ARGUMENTS}"
 
-if [[ ${USE_TFTRT} != "0" ]]; then
-      COMMAND="${COMMAND} \
-          --use_tftrt \
-          --optimize_offline \
-          --precision ${TFTRT_PRECISION} \
-          --minimum_segment_size ${MIN_SEGMENT_SIZE} \
-        ${USE_DYNAMIC_SHAPE_FLAG} \
-          --max_workspace_size ${MAX_WORKSPACE_SIZE}"
-fi
+COMMAND=$(echo "${COMMAND}" | tr -s " ")
 
 echo -e "**Executing:**\n\n${COMMAND}\n"
 sleep 5
