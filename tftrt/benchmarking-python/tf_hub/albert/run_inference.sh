cbf528efbd46db96eb2b63410e18c059832b64ca
Pavani Majety <pavanimajety@gmail.com>
2022-06-23 15:25:35 -0700

    initial albert commit

---
+++ tftrt/benchmarking-python/tf_hub/albert/run_inference.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+nvidia-smi
+
+set -x
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+python ${BASE_DIR}/infer.py \
+    --data_dir=/path/to/script \
+    --input_saved_model_dir=/path/to/saved_model \
+    --batch_size=<BATCH_SIWZE> \
+    --output_tensors_name="logits,probs" \
+    `# The following is set because we will be running synthetic benchmarks` \
+    --total_max_samples=1000 \
+    --use_synthetic_data  \
+    --num_iterations=1000  \
+    ${@}
