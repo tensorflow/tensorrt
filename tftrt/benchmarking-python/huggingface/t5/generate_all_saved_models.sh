b5e98e7796b493fcd56abcafab646a0ef827a232
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2022-06-27 20:37:01 -0400

    [Benchmark Py]  HF T5 added

---
+++ tftrt/benchmarking-python/huggingface/t5/generate_all_saved_models.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/bash
+set -x
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd /tmp
+
+git lfs version || exit_code=$?
+if [[ ${exit_code} -ne 0 ]]; then
+    echo "Installing Git LFS"
+    wget https://github.com/git-lfs/git-lfs/releases/download/v3.2.0/git-lfs-linux-amd64-v3.2.0.tar.gz
+    tar -xzf git-lfs-linux-amd64-v3.2.0.tar.gz
+    bash $(pwd)/git-lfs-3.2.0/install.sh
+else
+    echo "Git LFS already installed"
+fi
+
+
+pip install -U transformers[sentencepiece]
+
+cd ${SCRIPT_DIR}
+
+T5_MODELS=(
+    "t5-small"
+    "t5-base"
+    "t5-large"
+    "google/t5-v1_1-base"
+)
+
+for MODEL_NAME in "${T5_MODELS[@]}"; do
+    echo "---------------------------------------------------------------------"
+    echo "Processing Model: ${MODEL_NAME} ..."
+
+    MODEL_DIR=${SCRIPT_DIR}/${MODEL_NAME#*/}   # Get the substring after "/"
+
+    if [[ ! -d "${MODEL_DIR}" ]]; then
+        echo "${MODEL_DIR} does NOT exist on your filesystem."
+        git clone https://huggingface.co/${MODEL_NAME}
+    else
+        echo "${MODEL_DIR} exist on your filesystem."
+    fi
+
+    cd ${MODEL_DIR}
+    git lfs pull
+    cd ${SCRIPT_DIR}
+
+    OUTPUT_DIR="${MODEL_DIR}/saved_models"
+    rm -rf ${OUTPUT_DIR}
+
+    sleep 2
+
+    python generate_saved_models.py \
+        --model_name=${MODEL_NAME} \
+        --output_directory=${OUTPUT_DIR}
+done
---
+++ tftrt/benchmarking-python/huggingface/t5/generate_saved_model.py
@@ -0,0 +1,74 @@
+import argparse
+import os
+import shutil
+
+from pprint import pprint
+
+import tensorflow as tf
+
+from transformers import AutoTokenizer
+from transformers import TFAutoModelWithLMHead
+from transformers import TFAutoModelForSeq2SeqLM
+
+SUPPORTED_MODELS = [
+    "t5-small",
+    "t5-base",
+    "t5-large",
+    "google/t5-v1_1-base"
+]
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="T5 SavedModel Converter")
+
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        required=True,
+        choices=SUPPORTED_MODELS,
+        help="The name of the model to convert to SavedModel."
+    )
+
+    parser.add_argument(
+        "--output_directory",
+        type=str,
+        required=True,
+        help="Directory containing where to export the saved model."
+    )
+
+    args = parser.parse_args()
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    if "google" in args.model_name:
+        model = TFAutoModelForSeq2SeqLM.from_pretrained(args.model_name)
+
+    else:
+        model = TFAutoModelWithLMHead.from_pretrained(args.model_name)
+
+    try:
+        shutil.rmtree(args.output_directory)
+    except OSError: pass
+
+    model_dir = os.path.join(args.output_directory, "model")
+    tokenizer_dir = os.path.join(args.output_directory, "tokenizer")
+
+    print(f"\n[INFO] Saving `{args.model_name} model` in directory: `{model_dir}`.")
+
+    tf.keras.models.save_model(
+        model=model,
+        filepath=model_dir,
+        overwrite=True,
+        include_optimizer=False,
+        save_format="tf",
+        signatures=model.serving,
+        options=None,
+        save_traces=True
+    )
+
+    print(f"\n[INFO] Saving `{args.model_name} tokenizer` in directory: `{tokenizer_dir}`.")
+
+    # Reload with `AutoTokenizer.from_pretrained(tokenizer_dir)`
+    data = tokenizer.save_pretrained(tokenizer_dir)
+    print("[INFO] Tokenizer Data Saved:")
+    pprint(data)
