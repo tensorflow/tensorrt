23914ee19975ac0979c2f72b858a6c1f38fcd8e8
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2022-07-18 10:13:46 -0700

    YAPF Formatting Updated

--- tests/utils.py
+++ tests/utils.py
@@ -8,8 +8,8 @@ from contextlib import contextmanager
 from glob import glob, iglob
 
 __all__ = [
-    'CustomTestCase',
-    'list_all_py_files',
+    "CustomTestCase",
+    "list_all_py_files",
 ]
 
 
@@ -20,14 +20,17 @@ class CustomTestCase(unittest.TestCase):
         try:
             yield None
         except exc_type:
-            raise self.failureException('{} raised'.format(exc_type.__name__))
+            raise self.failureException(f"{exc_type.__name__} raised")
 
 
 _excludes_paths = ["tftrt/blog_posts/"]
 
 
 def list_all_py_files():
-    for _dir in ['tests', 'tftrt']:
+    for _dir in [
+        "tests",
+        os.path.join("tftrt", "benchmarking-python")
+    ]:
         for _file in iglob(f"{_dir}/**/*.py", recursive=True):
             if any([path in _file for path in _excludes_paths]):
                 continue
