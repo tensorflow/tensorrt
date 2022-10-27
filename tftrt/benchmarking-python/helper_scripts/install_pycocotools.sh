d4765fc87c7c652fdb7449c84ac7605ca1017486
Nathan Luehr <nluehr@nvidia.com>
2022-10-27 13:39:44 -0500

    Bump cocoapi to v0.7.2 (#329)

--- tftrt/benchmarking-python/helper_scripts/install_pycocotools.sh
+++ tftrt/benchmarking-python/helper_scripts/install_pycocotools.sh
@@ -7,6 +7,6 @@ DEPENDENCIES_STATUS=$?
 
 if [[ ${DEPENDENCIES_STATUS} != 0 ]]; then
     # Master Branch of 2022/01/11
-    PYCOCOTOOLS_BRANCH_OR_COMMIT_ID="v0.7.1"
+    PYCOCOTOOLS_BRANCH_OR_COMMIT_ID="v0.7.2"
     pip install "git+https://github.com/NVIDIA/cocoapi.git@${PYCOCOTOOLS_BRANCH_OR_COMMIT_ID}#egg=pycocotools&subdirectory=PythonAPI"
 fi
